fef904146c2cf04640c89e43c5bbeff173ebf287
Rashique65 <rashiq.2012@gmail.com>
2021-06-09 14:30:20 +0530

    Initial commit from gatsby: (https://github.com/chrisdesilva/gatsby-contentful-little-league.git)

---
+++ gatsby-node.js
@@ -0,0 +1,38 @@
+const path = require("path")
+const slash = require("slash")
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return graphql(
+    `
+      {
+        allContentfulTeam {
+          edges {
+            node {
+              slug
+            }
+          }
+        }
+      }
+    `
+  )
+    .then(result => {
+      if (result.errors) {
+        console.log("Error with contentful data", result.errors)
+      }
+
+      const teamTemplate = path.resolve("./src/templates/team.js")
+
+      result.data.allContentfulTeam.edges.forEach(team => {
+        createPage({
+          path: `/teams/${team.node.slug}/`,
+          component: slash(teamTemplate),
+          context: {
+            slug: team.node.slug,
+          },
+        })
+      })
+    })
+    .catch(error => console.log("Error with contentful data", error))
+}
